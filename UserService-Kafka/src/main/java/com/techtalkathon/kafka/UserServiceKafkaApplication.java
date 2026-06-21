d9c934275086c58e9c0c417df1106e0690f803ad
MalaBabu <allubabu1998@gmail.com>
2026-06-21 19:57:44 +0530

    Adding User service using Kafka.

---
+++ UserService-Kafka/src/main/java/com/techtalkathon/kafka/UserServiceKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.techtalkathon.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceKafkaApplication.class, args);
+	}
+
+}
